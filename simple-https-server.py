4a4aeee664acebfb6cd8017b11e56f535675692e
shawn.sarwar@ehealthnigeria.org <shawn.sarwar@ehealthnigeria.org>
2017-05-16 16:00:43 -0400

    took testing cert out and added warnings

--- simple-https-server.py
+++ simple-https-server.py
@@ -9,6 +9,15 @@
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
+
+print "this is only for testing, but is broadcasting on 443, so be careful."
+
 httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
-httpd.serve_forever()
+try:
+    httpd.serve_forever()
+except IOError, e:
+    print "you need to create a cert called server.pem and stick it in this folder."
+    print "this should do the trick:"
+    print "openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes"
+    print "see: https://gist.github.com/dergachev/7028596 for more"
